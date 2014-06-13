57f1234353b336de1ebcff02086912c69bf5320f
Painted <will.allen@paintedltd.co.uk>
2014-06-13 11:48:24 +0100

    Added ability to view and delete previsous ideas

--- app.rb
+++ app.rb
@@ -1,26 +1,28 @@
 require './idea'
 
 class IdeaBoxApp < Sinatra::Base
+	set :method_override, true
 	configure :development do 
 		register Sinatra::Reloader
 	end
 
 	get '/' do
-		# "<h1>Hello, World!</h1><blockquote>I guess I always felt even if the world came to an end, McDonald's would still be open. <cite>Susan Beth Pfeffer</cite></blockquote>"
-		erb :index
+		erb :index, locals: {ideas: Idea.all}
 	end
 
 	post '/' do 
-	# 	# 1. Create an idea based on the form parameters
 	 	idea = Idea.new(params['idea_title'], params['idea_description'])
-	# 	# 2. Store it
 		idea.save
-	# 	# 3. Send us back to the idex page to see all ideas
-		"Creating an IDEA"
+		redirect'/'
 	end
 
 	not_found do
 		erb :error
+	end
+
+	delete '/:id' do |id|
+		Idea.delete(id.to_i)
+		redirect '/'
 	end
 
 end
--- idea.rb
+++ idea.rb
@@ -2,10 +2,30 @@ require 'yaml/store'
 
 class Idea
 	attr_reader :title, :description
-	
+
 	def initialize(title, description)
 		@title = title
 		@description = description
+	end
+
+	def self.all 
+		raw_ideas.map do |data|
+			new(data[:title], data[:description])
+		end
+	end
+
+	def self.raw_ideas
+		database.transaction do |db|
+			db['ideas'] || []
+		end
+	end
+
+	def self.database
+		@database ||= YAML::Store.new('ideabox')
+	end
+
+	def database
+		Idea.database
 	end
 
 	def save
@@ -17,6 +37,12 @@ class Idea
 
 	def database
 		@database ||= YAML::Store.new "ideabox"
+	end
+
+	def self.delete(position)
+		database.transaction do 
+			database['ideas'].delete_at(position)
+		end
 	end
 
 end
